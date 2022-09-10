b5e4939c00dda670adc536a2aee93f2c6d071181
DEVANG KUSHWAH <67060334+DEVANGKUSHWAH@users.noreply.github.com>
2022-09-11 04:20:33 +0530

    register api integration and epic creation done

--- src/Reducers/reducers.js
+++ src/Reducers/reducers.js
@@ -1,12 +1,15 @@
 import {ActionTypes} from  '../Action/actions';
 export const initialState = {
-    loggedInUser:0
+    loggedInUser:"",
+    registeredUser:""
 }
 export function authReducer(state = initialState, action){
-    switch(action.type){
-        case ActionTypes.LOG_IN:
-            return {...state,loggedInUser:state.loggedInUser+1}
-        default:
-            return state
+    switch (action.type) {
+      case ActionTypes.LOG_IN_SUCCESS:
+        return { ...state, registeredUser: action.payload };
+      case ActionTypes.LOG_IN_ERROR:
+        return { ...state, registeredUser: action.payload };
+      default:
+        return state;
     }
 }
